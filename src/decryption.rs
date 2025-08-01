9600ca6af8a58e7ceb12926c7ca2fc940885b44c
4rmanc <ozgurarmancyigit@hotmail.com>
2025-08-01 10:16:40 +0300

    wip decrypt

--- src/decryption.rs
+++ src/decryption.rs
@@ -1,9 +1,9 @@
-use std::sync::Arc;
-
 use crate::encryption::PvwCiphertext;
 use crate::params::{PvwError, PvwParameters, Result};
 use crate::secret_key::SecretKey;
 use fhe_math::rq::{Context, Poly, Representation};
+use num_traits::ToPrimitive;
+use std::sync::Arc;
 
 /// Decrypt a PVW ciphertext to recover the plaintext
 pub fn decrypt(
@@ -11,10 +11,11 @@ pub fn decrypt(
     sk: &SecretKey,
     ctx: &Arc<Context>,
     ct: &PvwCiphertext,
-) -> Result<Vec<i64>> {
+) -> Result<Poly> {
     let q = &params.q;
     let k = params.k;
     let l = params.l;
+    let g = params.gadget_vector()?;
 
     // Compute y = c2 - (c1 * s)
     if ct.c1.len() != k || sk.coeff_matrix.len() != k {
@@ -29,7 +30,7 @@ pub fn decrypt(
         let sk_vec = sk.coeff_matrix[i].clone();
         for j in 0..l {
             let mut sk_i_poly = Poly::from_coefficients(&[sk_vec[j]], ctx).map_err(|e| {
-                PvwError::InvalidParameters(format!("Failed to create m polynomial: {}", e))
+                PvwError::InvalidParameters(format!("Failed to create sk polynomial: {}", e))
             })?;
             sk_i_poly.change_representation(Representation::Ntt);
 
@@ -39,5 +40,26 @@ pub fn decrypt(
         y.push(&ct.c2[i] - &inner_product.clone());
     }
 
-    Ok(sk.coeff_matrix[0].clone())
+    let mut z = Poly::zero(ctx, Representation::Ntt);
+    for i in 0..g.len() {
+        // Create polynomial for x[i]
+        let mut g_i_poly =
+            Poly::from_coefficients(&[g[i].to_i64().unwrap()], ctx).map_err(|e| {
+                PvwError::InvalidParameters(format!("Failed to create g_i polynomial: {}", e))
+            })?;
+        g_i_poly.change_representation(Representation::Ntt);
+
+        z += &(&g_i_poly * &y[i]);
+    }
+
+    let mut g_0_poly = Poly::from_coefficients(&[g[0].modinv(q).unwrap().to_i64().unwrap()], ctx)
+        .map_err(|e| {
+        PvwError::InvalidParameters(format!("Failed to create g_0 polynomial: {}", e))
+    })?;
+    g_0_poly.change_representation(Representation::Ntt);
+    let e = z;
+
+    let pt = &(&y[0] - &e) * &g_0_poly;
+
+    Ok(pt)
 }
